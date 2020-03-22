e47d40376926cd5586db34618e01e65ea8aaa6ee
Yauheni Kozak <43273391+Zhenikusss@users.noreply.github.com>
2020-03-22 23:39:14 +0300

    feat: implement function brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBrackets = bracketsConfig.map(x => x[0]);
+    let closeBrackets = bracketsConfig.map(x => x[1]);
+    let stack = [];
+
+    for (let ch of str) {
+        if (stack.length != 0) {
+            let closeBracket = stack[stack.length - 1];
+            if (ch == closeBracket) {
+                stack.pop();
+                continue;
+            }
+        }
+
+        let bracketIndex = openBrackets.indexOf(ch);
+        if (bracketIndex != -1) {
+            let closeBracket = closeBrackets[bracketIndex];
+            stack.push(closeBracket);
+        } else {
+            return false;
+        }
+    }
+    return stack.length == 0;
 }
